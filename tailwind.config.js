1781bdf963af31b7c7aa9d1fb5d5bd168dea3d61
Joanna Skubisz <joannaskubisz@MacBook-Air-Joanna.local>
2026-05-18 07:11:37 +0200

    gotowe

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8b5cf6', // Twój własny kolor z zadania dodatkowego
+      }
+    },
+  },
+  plugins: [],
+}
